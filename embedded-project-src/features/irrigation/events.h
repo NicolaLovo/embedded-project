0afe3a7b93521c397d6f30e6871180b9aa971b92
martinadep <depiccolimartina26@gmail.com>
2024-11-27 15:17:52 +0100

    few changes on irrigation files

--- embedded-project-src/features/irrigation/events.h
+++ embedded-project-src/features/irrigation/events.h
@@ -1,10 +1,13 @@
 #ifndef __IRRIGATION_EVENTS_H
 #define __IRRIGATION_EVENTS_H
 
-// Called when temp sensor is high (above a threshold)
+#define LOW_TEMP 20
+#define HIGH_TEMP 30
+
+// Called when temp sensor is high (above 30)
 void irrigation_event_highTemp(void);
 
-// Called when temp sensor is low (under a threshold)
+// Called when temp sensor is low (under 20)
 void irrigation_event_lowTemp(void);
 
 // Called when the irrigation is on
