62a3a2a263fbae02e9266120e3742413bf61d9c7
NicolaLovo <nicolalovo@gmail.com>
2024-11-27 15:05:16 +0100

    Improve readabilty of door feature

--- embedded-project-src/features/door/events.c
+++ embedded-project-src/features/door/events.c
@@ -4,52 +4,36 @@
 
 
 void door_event_skyDark(void) {
-    switch(door_current_state) {
-    case DOOR_STATE_CLOSE:
-    case DOOR_STATE_FORCE_OPEN:
-        return;
-    case DOOR_STATE_OPEN:
+    if (door_current_state == DOOR_STATE_OPEN) {
         door_current_state = DOOR_STATE_CLOSE;
         return;
     }
 }
-void door_event_button_open(void){
-    switch(door_current_state) {
-    case DOOR_STATE_OPEN:
-    case DOOR_STATE_FORCE_OPEN:
-        return;
-    case DOOR_STATE_CLOSE:
+
+void door_event_button_open(void) {
+    if (door_current_state == DOOR_STATE_CLOSE) {
         door_current_state = DOOR_STATE_OPEN;
         return;
     }
 }
+
 void door_event_button_close(void) {
-    switch(door_current_state) {
-    case DOOR_STATE_CLOSE:
-        return;
-    case DOOR_STATE_FORCE_OPEN:
-    case DOOR_STATE_OPEN:
+    if (door_current_state == DOOR_STATE_FORCE_OPEN || door_current_state == DOOR_STATE_OPEN) {
         door_current_state = DOOR_STATE_CLOSE;
         return;
     }
 }
+
 void door_event_earthquake_start(void) {
-    switch(door_current_state) {
-    case DOOR_STATE_FORCE_OPEN:
-        return;
-    case DOOR_STATE_CLOSE:
-    case DOOR_STATE_OPEN:
+    if (door_current_state == DOOR_STATE_CLOSE || door_current_state == DOOR_STATE_OPEN) {
         door_current_state = DOOR_STATE_FORCE_OPEN;
         return;
     }
 }
+
 void door_event_earthquake_end(void) {
-    switch(door_current_state) {
-    case DOOR_STATE_FORCE_OPEN:
+    if (door_current_state == DOOR_STATE_FORCE_OPEN) {
         door_current_state = DOOR_STATE_OPEN;
-        return;
-    case DOOR_STATE_CLOSE:
-    case DOOR_STATE_OPEN:
         return;
     }
 }
