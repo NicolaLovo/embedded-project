77b5ea7e62c638828ed7b0f263a5212b4a0ee538
Cristiano Berardo <75799096+CristianoBerardo@users.noreply.github.com>
2024-12-01 18:04:45 +0100

    Some changes on allarm and front_light_door

--- embedded-project-src/features/allarm/events.c
+++ embedded-project-src/features/allarm/events.c
@@ -1,51 +1,30 @@
 #include "fsm.h"
 #include "events.h"
 
-void allarm_event_onContact(void) {
-    switch (allarm_current_state) {
-    case ALLARM_STATE_IDLE:
-    case ALLARM_STATE_INIT:
-    case ALLARM_STATE_BUZZER_ACTIVE:
-    case ALLARM_STATE_ACTIVE:
+void allarm_event_onContact(void){
+
+    if (allarm_current_state == ALLARM_STATE_ACTIVE){
         allarm_current_state = ALLARM_STATE_BUZZER_ACTIVE;
-        return;
     }
 }
 
-void allarm_event_onContactEnd(void) {
-    switch (allarm_current_state) {
+void allarm_event_onContactEnd(void){
 
-
-    case ALLARM_STATE_ACTIVE:
-    case ALLARM_STATE_INIT:
-    case ALLARM_STATE_IDLE:
-    case ALLARM_STATE_BUZZER_ACTIVE:
+    if (allarm_current_state == ALLARM_STATE_BUZZER_ACTIVE){
         allarm_current_state = ALLARM_STATE_IDLE;
-        return;
     }
 }
 
-void allarm_event_skyDark(void) {
-    switch (allarm_current_state) {
+void allarm_event_skyDark(void){
 
-    case ALLARM_STATE_ACTIVE:
-    case ALLARM_STATE_INIT:
-    case ALLARM_STATE_BUZZER_ACTIVE:
-    case ALLARM_STATE_IDLE:
-        return allarm_current_state = ALLARM_STATE_ACTIVE;
+    if (allarm_current_state == ALLARM_STATE_IDLE){
+        allarm_current_state = ALLARM_STATE_ACTIVE;
     }
 }
 
-void allarm_event_skyLight(void) {
-    switch (allarm_current_state) {
-    case ALLARM_STATE_IDLE:
-    case ALLARM_STATE_INIT:
-    case ALLARM_STATE_BUZZER_ACTIVE:
-    case ALLARM_STATE_ACTIVE:
-        return allarm_current_state = ALLARM_STATE_IDLE;
+void allarm_event_skyLight(void){
+    
+    if (allarm_current_state == ALLARM_STATE_ACTIVE){
+        allarm_current_state = ALLARM_STATE_IDLE;
     }
 }
-
-
-
-
