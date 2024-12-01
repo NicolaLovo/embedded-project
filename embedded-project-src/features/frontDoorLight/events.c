7f133bed58f70a8d98d33d07f1c776ef1b12799b
Cristiano Berardo <75799096+CristianoBerardo@users.noreply.github.com>
2024-12-01 18:04:45 +0100

    Some changes on allarm and front_light_door

--- embedded-project-src/features/frontDoorLight/events.c
+++ embedded-project-src/features/frontDoorLight/events.c
@@ -1,28 +1,16 @@
 #include "events.h"
 #include "fsm.h"
 
-void front_door_light_event_skyDark(void) {
+void front_door_light_event_skyDark(void){
 
-    switch (front_door_current_state) {
-    case FRONT_DOOR_LIGHT_STATE_OFF:
+    if (front_door_current_state == FRONT_DOOR_LIGHT_STATE_OFF){
         front_door_current_state = FRONT_DOOR_LIGHT_STATE_ON;
-        return;
-    case FRONT_DOOR_LIGHT_STATE_ON:
-    case FRONT_DOOR_LIGHT_STATE_INIT:
-        return;
     }
-
 }
 
-void front_door_light_event_skyLight(void) {
+void front_door_light_event_skyLight(void){
 
-    switch (front_door_current_state) {
-    case FRONT_DOOR_LIGHT_STATE_ON:
+    if (front_door_current_state == FRONT_DOOR_LIGHT_STATE_ON){
         front_door_current_state = FRONT_DOOR_LIGHT_STATE_OFF;
-        return;
-    case FRONT_DOOR_LIGHT_STATE_OFF:
-    case FRONT_DOOR_LIGHT_STATE_INIT:
-        return;
     }
-
 }
