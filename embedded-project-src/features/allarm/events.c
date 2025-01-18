899f8a5e8b44bc74dff56f5461c1441933dc4136
Cristiano Berardo <75799096+CristianoBerardo@users.noreply.github.com>
2025-01-19 00:08:23 +0100

    Start allarm testing

--- embedded-project-src/features/allarm/events.c
+++ embedded-project-src/features/allarm/events.c
@@ -7,10 +7,7 @@ void allarm_event_onContact(void) {
 
   if (allarm_current_state == ALLARM_STATE_ACTIVE) {
     allarm_current_state = ALLARM_STATE_BUZZER_ACTIVE;
-
-    return;
   }
-
   // printf("\nallarm_current_state: allarm_event_onContact");
 }
 
@@ -18,10 +15,7 @@ void allarm_event_onContactEnd(void) {
 
   if (allarm_current_state == ALLARM_STATE_BUZZER_ACTIVE) {
     allarm_current_state = ALLARM_STATE_IDLE;
-
     // printf("\nallarm_current_state: allarm_event_onContactEnd");
-
-    return;
   }
 }
 
@@ -30,7 +24,6 @@ void allarm_event_skyDark(void) {
   if (allarm_current_state == ALLARM_STATE_IDLE) {
     allarm_current_state = ALLARM_STATE_ACTIVE;
     // printf("\nallarm_current_state:  allarm_event_skyDark");
-    return;
   }
 }
 
@@ -39,6 +32,5 @@ void allarm_event_skyLight(void) {
   if (allarm_current_state == ALLARM_STATE_ACTIVE) {
     allarm_current_state = ALLARM_STATE_IDLE;
     // printf("\nallarm_current_state:   allarm_event_skyLight");
-    return;
   }
 }
--- embedded-project-src/testing/tests.c
+++ embedded-project-src/testing/tests.c
@@ -1,20 +1,19 @@
 #include "config.h"
 
-
 #ifdef TESTING
 
+#include "testing/allarm/tests.h"
 #include "testing/door/tests.h"
 
 void run_tests() {
-    test_door_feature();
+  test_door_feature();
+  test_allarm_feature();
 }
 
 #else
 
-
 void run_tests() {
-    // define as nothing
-
+  // define as nothing
 }
 
 #endif
