c66d42423607f40d313ad68aad3c13f7a0efba51
NicolaLovo <nicolalovo@gmail.com>
2025-01-18 09:36:44 +0100

    Fix testing for door

--- embedded-project-src/testing/tools.h
+++ embedded-project-src/testing/tools.h
@@ -5,6 +5,12 @@
 #ifndef TESTING
     #define assert(bool_expression) ((void)0) /* Define it as a no-op */
 #else
+
+void bad_assertion(const char *file, int line, const char *expression) {
+    printf("Assertion failed: (%s), file %s, line %d\n", expression, file, line);
+    exit(1);
+}
+
 #define assert(bool_expression) \
     if (!(bool_expression)) bad_assertion(__FILE__,__LINE__, #bool_expression);
 #endif
