b742b94a1a71a6a7bc8b80232ef2e5d57ab526a7
marti <martina.depiccoli2003@gmail.com>
2025-01-18 14:12:35 +0100

    added LCD graphics for earthquake detection

--- embedded-project-src/sensors/accelerometer/sensor_on_read.c
+++ embedded-project-src/sensors/accelerometer/sensor_on_read.c
@@ -5,15 +5,16 @@
 #include "features/door/fsm.h"
 #include "features/door/events.h"
 
+volatile extern EarthquakeState state = IDLE;
+
 void accelerometer_on_read(uint16_t resultsBuffer[3]) {
 
     uint16_t x = resultsBuffer[0];
     uint16_t y = resultsBuffer[1];
     uint16_t z = resultsBuffer[2];
 
-    static EarthquakeState state = IDLE;
     static int stabilityCounter = 0;
-
+    //static EarthquakeState state = IDLE;
     /* Check for significant movement */
             bool significantMovement = (x > HIGH_THRSD || y > HIGH_THRSD || z > HIGH_THRSD) ||
                                        (x < LOW_THRSD || y < LOW_THRSD || z < LOW_THRSD);
@@ -24,7 +25,7 @@ void accelerometer_on_read(uint16_t resultsBuffer[3]) {
                 if (significantMovement) {
                     state = ACTIVE;
                     stabilityCounter = 0;
-                    //printf("\n--- EARTHQUAKE START detected ---\n");
+                    printf("\n--- EARTHQUAKE START detected ---\n");
                     //printf("X: %u, Y: %u, Z: %u\n", x, y, z);
                     /* Trigger door event */
                     door_event_earthquake_start();
@@ -40,7 +41,7 @@ void accelerometer_on_read(uint16_t resultsBuffer[3]) {
                     if (stabilityCounter >= STABILITY_COUNT) {
                         state = STABILIZING;
                         stabilityCounter = 0;
-                        //printf("--- EARTHQUAKE END detected ---\n");
+                        printf("--- EARTHQUAKE END detected ---\n");
                         /* Trigger door event */
                         door_event_earthquake_end();
 
@@ -56,4 +57,12 @@ void accelerometer_on_read(uint16_t resultsBuffer[3]) {
                 break;
             }
 
+}
+bool earthquake_active(){
+  switch(state){
+     case IDLE: return false;
+     case ACTIVE: return true;
+     case STABILIZING: return true;
+  }
+  return false;
 }
