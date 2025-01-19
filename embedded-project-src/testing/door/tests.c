e53ca4064bf50017233841a42c100970900cc526
Cristiano Berardo <75799096+CristianoBerardo@users.noreply.github.com>
2025-01-19 21:04:58 +0100

    Fixing

--- embedded-project-src/testing/door/tests.c
+++ embedded-project-src/testing/door/tests.c
@@ -1,7 +1,7 @@
 #include "config.h"
-#include "testing/tools.h"
-#include "features/door/fsm.h"
 #include "features/door/events.h"
+#include "features/door/fsm.h"
+#include <assert.h>
 
 #ifdef TESTING
 
@@ -9,64 +9,60 @@
  * Run the current door fsm function
  */
 void run_door_fsm() {
-    if(door_current_state < DOOR_NUM_STATES){
-        (*door_fsm[door_current_state].state_function)();
-    }
+  if (door_current_state < DOOR_NUM_STATES) {
+    (*door_fsm[door_current_state].state_function)();
+  }
 }
 
 void test_door_feature() {
-    /**
-     * verify door is initialized
-     */
-    assert(door_current_state == DOOR_STATE_INIT);
-    run_door_fsm();
+  /**
+   * verify door is initialized
+   */
+  assert(door_current_state == DOOR_STATE_INIT);
+  run_door_fsm();
 
-    /**
-     * verify door is open after being initialized
-     */
-    assert(door_current_state == DOOR_STATE_OPEN);
-    run_door_fsm();
+  /**
+   * verify door is open after being initialized
+   */
+  assert(door_current_state == DOOR_STATE_OPEN);
+  run_door_fsm();
 
+  /**
+   * trigger event: sky dark -> expect door closed
+   */
+  door_event_skyDark();
+  assert(door_current_state == DOOR_STATE_CLOSE);
+  run_door_fsm();
 
-    /**
-     * trigger event: sky dark -> expect door closed
-     */
-    door_event_skyDark();
-    assert(door_current_state == DOOR_STATE_CLOSE);
-    run_door_fsm();
+  /**
+   * trigger event: button open -> expect door open
+   */
+  door_event_button_force_open();
+  assert(door_current_state == DOOR_STATE_FORCE_OPEN);
+  run_door_fsm();
 
-    /**
-     * trigger event: button open -> expect door open
-     */
-    door_event_button_force_open();
-    assert(door_current_state == DOOR_STATE_FORCE_OPEN);
-    run_door_fsm();
+  /**
+   * trigger event: button close -> expect door closed
+   */
+  door_event_button_close();
+  assert(door_current_state == DOOR_STATE_CLOSE);
+  run_door_fsm();
 
-    /**
-     * trigger event: button close -> expect door closed
-     */
-    door_event_button_close();
-    assert(door_current_state == DOOR_STATE_CLOSE);
-    run_door_fsm();
+  /**
+   * trigger event: earthquake -> expect door force open
+   */
+  door_event_earthquake_start();
+  assert(door_current_state == DOOR_STATE_FORCE_OPEN);
+  run_door_fsm();
 
+  /**
+   * trigger event: earthquake end -> expect door open
+   */
+  door_event_earthquake_end();
+  assert(door_current_state == DOOR_STATE_OPEN);
+  run_door_fsm();
 
-    /**
-     * trigger event: earthquake -> expect door force open
-     */
-    door_event_earthquake_start();
-    assert(door_current_state == DOOR_STATE_FORCE_OPEN);
-    run_door_fsm();
-
-    /**
-     * trigger event: earthquake end -> expect door open
-     */
-    door_event_earthquake_end();
-    assert(door_current_state == DOOR_STATE_OPEN);
-    run_door_fsm();
-
-    printf("Door tests done successully\n");
-
-
+  printf("Door tests done successully\n");
 }
 
 #else
