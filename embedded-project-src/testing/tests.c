a7052c9e06f038f4e99a5d747e639b1cf3025e97
marti <martina.depiccoli2003@gmail.com>
2025-01-22 14:50:07 +0100

    added irrigation tests

--- embedded-project-src/testing/tests.c
+++ embedded-project-src/testing/tests.c
@@ -4,10 +4,12 @@
 
 #include "testing/allarm/tests.h"
 #include "testing/door/tests.h"
+#include "testing/irrigation/tests.h"
 
 void run_tests() {
   test_door_feature();
   test_allarm_feature();
+  test_irrigation_feature();
 }
 
 #else
