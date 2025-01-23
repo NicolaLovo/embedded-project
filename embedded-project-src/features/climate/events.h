4c372f47b991fa494fd3f11752e05dc01d026ba7
NicolaLovo <nicolalovo@gmail.com>
2025-01-23 08:30:28 +0100

    Fixed compilation errors

--- embedded-project-src/features/climate/events.h
+++ embedded-project-src/features/climate/events.h
@@ -1,20 +1,16 @@
-#ifndef FEATURES_CLIMATE_EVENTS_H_
-#define FEATURES_CLIMATE_EVENTS_H_
+#ifndef __CLIMATE_EVENTS_H
+#define __CLIMATE_EVENTS_H
 
 #define CLIMATE_LOWEST_TEMPERATURE 19
 #define CLIMATE_HIGHEST_TEMPERATURE 25
 #define CLIMATE_SHUTOFF_TEMPERATURE 22
 
 
-v#ifndef FEATURES_EVENTS_H_
-#define FEATURES_EVENTS_H_
 
 void climate_event_under19(void);
 void climate_event_above22(void);
 void climate_event_above25(void);
 void climate_event_under22(void);
-
-#endif
 
 
 
