e1bcdfea71483de5a60f15f0270479f01eac778e
NicolaLovo <nicolalovo@gmail.com>
2025-01-23 10:21:10 +0100

    End climate testing

--- embedded-project-src/features/climate/events.c
+++ embedded-project-src/features/climate/events.c
@@ -2,25 +2,15 @@
 #include "features/climate/fsm.h"
 
 void climate_on_under_low(){
-    if(climate_current_state==CLIMATE_STATE_OFF){
-        climate_current_state=CLIMATE_STATE_RADIATOR;
-    }
+    climate_current_state=CLIMATE_STATE_RADIATOR;
 
 }
 void climate_on_above_idle(){
-    if(climate_current_state==CLIMATE_STATE_RADIATOR){
-        climate_current_state=CLIMATE_STATE_OFF;
-    }
-
+    climate_current_state=CLIMATE_STATE_OFF;
 }
 void climate_on_above_high(){
-    if(climate_current_state==CLIMATE_STATE_OFF){
-        climate_current_state=CLIMATE_STATE_AIRCONDITIONING;
-    }
-
+    climate_current_state=CLIMATE_STATE_AIRCONDITIONING;
 }
 void climate_on_under_idle(){
-    if(climate_current_state==CLIMATE_STATE_AIRCONDITIONING){
-        climate_current_state=CLIMATE_STATE_OFF;
-    }
+    climate_current_state=CLIMATE_STATE_OFF;
 }
--- embedded-project-src/testing/climate/tests.c
+++ embedded-project-src/testing/climate/tests.c
@@ -22,6 +22,22 @@ void test_climate_feature() {
   assert(climate_current_state == CLIMATE_STATE_INIT);
   run_climate_fsm();
 
+  climate_on_under_low();
+  assert(climate_current_state == CLIMATE_STATE_RADIATOR);
+  run_climate_fsm();
+
+  climate_on_above_idle();
+  assert(climate_current_state == CLIMATE_STATE_OFF);
+  run_climate_fsm();
+
+  climate_on_above_high();
+  assert(climate_current_state == CLIMATE_STATE_AIRCONDITIONING);
+  run_climate_fsm();
+
+
+  climate_on_under_idle();
+  assert(climate_current_state == CLIMATE_STATE_OFF);
+  run_climate_fsm();
 
   printf("climate tests done successully\n");
 }
