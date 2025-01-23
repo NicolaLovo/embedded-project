22384af01b3b67cfef72a1f8bc404df37ed13131
Matilde Prati <matiprati@icloud.com>
2025-01-23 09:26:01 +0100

    Parte finale corretta con lovo

--- embedded-project-src/sensors/climate/sensor_on_read.c
+++ embedded-project-src/sensors/climate/sensor_on_read.c
@@ -1,32 +1,21 @@
 #include "sensor_on_read.h"
+#include "sensor_hw.h"
+#include "features/events.h"
 
 
-
-
-
-/*switch (state){
-
-    case OFF:
-        if (tempC > HIGHEST_TEMPERATURE){
-            fn_aria_condizionata();
-        }
-    case ON:
-        if (tempC ...){
-
-        }
-        
-}
-*/
-void climate_sensor_on_read_handleTemperature(void){
+void climate_sensor_on_read(void){
     int temperature= climate_sensor_hw_readTemperature();
-    if (temperature<LOWEST_TEMPERATURE){
-        climate_event_lowTemperature();
-
-    }else if(temperature>HIGHEST_TEMPERATURE){
-        climate_event_highTemperature();
-    }else if(temperature< CLIMATE_SHUTOFF_TEMPERATURE || temperature> CLIMATE_SHUTOFF_TEMPERATURE){
-        climate_event_turnOffTemperature();
+    if (temperature < LOWEST_TEMPERATURE) {
+        climate_event_under19();
+    } else if (temperature >= LOWEST_TEMPERATURE && temperature < SHUTOFF_TEMPERATURE) {
+        climate_event_above22();
+    } else if (temperature >= SHUTOFF_TEMPERATURE && temperature < HIGHEST_TEMPERATURE) {
+        climate_event_under22();
+    } else if (temperature >= HIGHEST_TEMPERATURE) {
+        climate_event_above25();
     }
+    // Add a delay for readability and to avoid busy waiting
+    sensor_delay_ms(1000);
     
 
 }
