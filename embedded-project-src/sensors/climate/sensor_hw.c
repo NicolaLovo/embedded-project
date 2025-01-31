e1a9af674afce53c53546199771f5390fc0b20c8
NicolaLovo <nicolalovo@gmail.com>
2025-01-31 09:53:27 +0100

    Delete legacy code on temperature sensor

--- embedded-project-src/sensors/climate/sensor_hw.c
+++ embedded-project-src/sensors/climate/sensor_hw.c
@@ -4,12 +4,6 @@
 #include "tools/HAL_TMP006.h" //Sensor specific driver TMP006
 #include <stdint.h>
 
-//sensor calibration
-uint32_t cal30;
-uint32_t cal85;
-//variables for conversion
-float calDifference;
-float tempC;
 
 void climate_sensor_hw_init(void){
     Init_I2C_GPIO(); //configure pin I2C
@@ -25,52 +19,3 @@ float climate_sensor_hw_readTemperature(void){
     float tempC = (tempFarenheit - 32) * 5.0/9.0;
     return tempC - 10;
 }
-
-//sensor interrupt handling 
-/*
-void ADC14_IRQHandler(void){
-    uint64_t status;
-    int16_t conRes;
-
-    status = ADC14_getEnabledInterruptStatus();
-    ADC14_clearInterruptFlag(status);
-
-    if (status & ADC_INT0)
-    {
-        conRes = ((ADC14_getResult(ADC_MEM0) - cal30) * 55);
-        tempC = (conRes / calDifference) + 30.0f;
-
-        // Turn on/off led based on the temperature
-        if (tempC < 19.0f)
-        {
-            GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN0); // turn on red
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);  // turn off green
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN2);  // turn ogg blue 
-        }
-        else if (tempC >= 19.0f && tempC <= 22.0f)
-        {
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);  // turn off red
-            GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1); // turn on green
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN2);  // turn off blue
-        }
-        else if (tempC > 22.0f && tempC <= 25.0f)
-        {
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);  // turn off red
-            GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN1); // turn on green
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN2);  // turn off blue
-        }
-        else if (tempC > 25.0f)
-        {
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);  // turn off red
-            GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN1);  // turn off green
-            GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN2); // turn on blue
-        }
-    }
-}
-
-*/
-
-
-
-
-
--- embedded-project-src/sensors/climate/sensor_hw.h
+++ embedded-project-src/sensors/climate/sensor_hw.h
@@ -8,32 +8,10 @@
 
 #define SENSOR_IRQn
 
-uint32_t cal30;
-uint32_t cal85;
-float calDifference;
-
 //init sensor
 void climate_sensor_hw_init(void);
 
 //read temperature of sensor 
 float climate_sensor_hw_readTemperature(void);
-
-//ISR: INTERRUPT
-// void ADC14_IRQHandler(void);
-
-/*
-MAIN 
-// Inizializzazione dell'hardware 
-    climate_sensor_hw_init();
-
-//     Mette il microcontrollore in modalità LPM0 per risparmiare energia 
-    while (1)
-    {
-        PCM_gotoLPM0();
-    }
-
-*/
-
-
 
 #endif
